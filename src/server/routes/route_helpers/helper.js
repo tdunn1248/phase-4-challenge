b41ae8d612e384f2950d65b4eb12e032fd75d6d1
tdunn1248 <tdunn1248@gmail.com>
2017-09-25 16:44:53 -0700

    handle errors

--- src/server/routes/route_helpers/helper.js
+++ src/server/routes/route_helpers/helper.js
@@ -2,7 +2,5 @@ const Album = require('../../../models/albums')
 const Review = require('../../../models/reviews');
 
 module.exports = {
-  AlbumsAndNewestReviews: () => {
-    return Promise.all([Album.readAll(), Review.readThreeNewest()])
-  }
+  AlbumsAndNewestReviews: () => Promise.all([Album.readAll(), Review.readThreeNewest()])
 }

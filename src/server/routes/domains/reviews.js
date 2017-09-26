eca92e8e083b2262c1274709aba2512bc94ce81c
tdunn1248 <tdunn1248@gmail.com>
2017-09-26 13:46:43 -0700

    indent

--- src/server/routes/domains/reviews.js
+++ src/server/routes/domains/reviews.js
@@ -3,7 +3,7 @@ const Review = require('../../../models/reviews')
 const {validateReviewForm} = require('../route_helpers/validation')
 
 router.post('/:id', validateReviewForm, (request, response) => {
-  Review.create( request.body.newReview, request.session.id, request.params.id)
+  Review.create(request.body.newReview, request.session.id, request.params.id)
   .then(() => {response.redirect('/albums/' + request.params.id)})
 })
 

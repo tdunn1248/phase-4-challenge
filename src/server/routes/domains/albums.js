d3b047c57e2f8a700c590b3b3523769b1171e94a
tdunn1248 <tdunn1248@gmail.com>
2017-09-25 20:45:23 -0700

    view helpers

--- src/server/routes/domains/albums.js
+++ src/server/routes/domains/albums.js
@@ -5,16 +5,12 @@ const {checkUserSession} = require('../route_helpers/session/check-user-session'
 
 router.get('/:id', (request, response) => {
   Album.infoAndReviews(request.params.id)
-  .then(albumInfo => {
-    response.status(200).render('albums/album', {view: albumView(request, albumInfo)})
-  })
+  .then(albumInfo => {response.status(200).render('albums/album', {view: albumView(request, albumInfo)})})
 })
 
 router.get('/:id/reviews/new', checkUserSession, (request, response) => {
   Album.readById(request.params.id)
-  .then(albumInfo => {
-    response.status(200).render('reviews/new-review', {view: newReviewView(request, albumInfo)})
-  })
+  .then(albumInfo => {response.status(200).render('reviews/new-review', {view: newReviewView(request, albumInfo)})})
 })
 
 module.exports = router
